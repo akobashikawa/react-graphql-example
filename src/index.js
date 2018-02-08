2f22e3d6a7ead35f43d486f0887c0ccdf8ec5ff0
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-08 17:26:50 -0500

    Setup apollo base

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
 import './index.css';
 import App from './components/App/';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// https://www.apollographql.com/docs/react/basics/setup.html
+const client = new ApolloClient({
+  // By default, this client will send queries to the
+  //  `/graphql` endpoint on the same host
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
